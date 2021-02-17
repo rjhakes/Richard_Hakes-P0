15a61bb67474d731ce8e923fb08750e5da8416b7
Rich Hakes <rjhakes@yahoo.com>
2021-02-17 07:25:23 -0700

    Init CustomerBL

---
+++ StoreApp/StoreBL/CustomerBL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using StoreDL;
+using StoreModels;
+
+namespace StoreBL
+{
+    public class CustomerBL : ICustomerBL
+    {
+        private ICustomerRepository repo = new CustomerRepoSC();
+        public void AddCustomer(Customer newCustomer)
+        {
+            //TODO: Add BL
+            repo.AddCustomer(newCustomer);
+        }
+        public List<Customer> GetCustomers()
+        {
+            //TODO Add BL
+            return repo.GetCustomers();
+        }
+    }
+}
