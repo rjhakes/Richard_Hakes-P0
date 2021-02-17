6b4b6998e1ec41fcadd4370910e9eca68e023b02
Rich Hakes <rjhakes@yahoo.com>
2021-02-17 07:31:03 -0700

    Added ToString override

--- StoreApp/StoreModels/Address.cs
+++ StoreApp/StoreModels/Address.cs
@@ -4,14 +4,17 @@ namespace StoreModels
 {
     public class Address
     {
-        private string street;
+        /*private string street;
         private string city;
         private string country;
-        private string postalCode;
+        private string postalCode;*/
 
         public string Street { get; set; }
         public string City { get; set; }   
+        public string State { get; set; }
         public string Country { get; set; }
         public string PostalCode { get; set; }
+
+        public override string ToString() => $"\n\t Street:\t {this.Street} \n\t City:\t\t {this.City} \n\t State:\t\t {this.State} \n\t Country:\t {this.Country} \n\t Postal Code:\t {this.PostalCode} \n\t";
     }
 }
