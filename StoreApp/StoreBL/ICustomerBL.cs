c0afcc6c113b4d86c4c63c8a5e9ecae5d18fa4c6
Rich Hakes <rjhakes@yahoo.com>
2021-02-17 07:25:44 -0700

    Init ICustomerBL

---
+++ StoreApp/StoreBL/ICustomerBL.cs
@@ -0,0 +1,11 @@
+using StoreModels;
+using System.Collections.Generic;
+
+namespace StoreBL
+{
+    public interface ICustomerBL
+    {
+         List<Customer> GetCustomers();
+         void AddCustomer(Customer newCustomer);
+    }
+}
