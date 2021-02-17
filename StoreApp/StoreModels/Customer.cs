8ff6b01874a8c7ed2c569d319eae267d6aa45ce6
Rich Hakes <rjhakes@yahoo.com>
2021-02-16 20:04:50 -0700

    Added fields to Customer Model

--- StoreApp/StoreModels/Customer.cs
+++ StoreApp/StoreModels/Customer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace StoreModels
 {
     /// <summary>
@@ -5,7 +7,53 @@ namespace StoreModels
     /// </summary>
     public class Customer
     {
-        public string Name { get; set; }
+        private string custName;
+        private string custEmail;
+        private int custPhoneNumber;
+        private Address custShipAddress;
+        private Address custBillAddress;
+        private List<Item> custCart;
+
+
+        public string CustName { 
+            get { return custName; } 
+            set {
+                if (value.Equals(null)) {
+                    //TODO: throw exception
+                }
+                custName = value;
+            } 
+        }
+
+        public string CustEmail { 
+            get {
+                return custEmail;
+            } 
+            set {
+                if (value.Equals(null)) {
+                    //TODO: throw Exception
+                }
+                custEmail = value;
+            }
+        
+        }
+
+        public int CustPhoneNumber { 
+            get {
+                return custPhoneNumber;
+            } 
+            set {
+                if (value.Equals(null)) {
+                    //TODO: throw exception
+                }
+                //format phone number
+                custPhoneNumber = value;
+            } 
+        }
+
+        public string CustShipAddress { get; set; }
+        public string CustBillAddress { get; set; }
+        public List<Item> CustCart { get; set; }
         //TODO: add more properties to identify the customer
     }
 }
