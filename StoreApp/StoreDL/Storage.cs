26d78a5fb13e6328caa9ba14d0c47c26bbcbee8c
Rich Hakes <rjhakes@yahoo.com>
2021-02-17 21:40:07 -0700

    added Lists

--- StoreApp/StoreDL/Storage.cs
+++ StoreApp/StoreDL/Storage.cs
@@ -6,5 +6,7 @@ namespace StoreDL
     public class Storage
     {
         public static List<Customer> AllCustomers = new List<Customer>();
+        public static List<Product> AllProducts = new List<Product>();
+        public static List<Location> AllLocations = new List<Location>();
     }
 }
