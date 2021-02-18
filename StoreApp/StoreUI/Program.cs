1eea54af29683149928e1323d04584fc9d78e700
Rich Hakes <rjhakes@yahoo.com>
2021-02-17 21:45:24 -0700

    startup menu

--- StoreApp/StoreUI/Program.cs
+++ StoreApp/StoreUI/Program.cs
@@ -1,65 +1,45 @@
-﻿using System;
+using System;
 using StoreModels;
 using StoreBL;
-
-
+using StoreDL;
+using System.Collections.Generic;
 namespace StoreUI
 {
     class Program
     {
-        /// <summary>
-        /// This is the main method, its the starting point of your application
-        /// </summary>
-        /// <param name="args"></param>
-
-        private static ICustomerBL customerBL = new CustomerBL();
-
         static void Main(string[] args)
-        {
-            //call method that starts main user interface
-            Customer newCustomer = new Customer();
-            Console.WriteLine("Enter Customer Name: ");
-            newCustomer.CustName = Console.ReadLine();
-            Console.WriteLine("Enter Customer Email (example@domain.com)");
-            newCustomer.CustEmail = Console.ReadLine();
-            Console.WriteLine("Enter Customer Phone # (1234567890):");
-            newCustomer.CustPhoneNumber = Console.ReadLine(); 
-            Console.WriteLine("Enter Customer's Shipping Address");
-            Address newAddress = new Address();
-            Console.WriteLine("Street:");
-            newAddress.Street = Console.ReadLine();
-            Console.WriteLine("City:");
-            newAddress.City = Console.ReadLine();
-            Console.WriteLine("State:");
-            newAddress.State = Console.ReadLine();
-            Console.WriteLine("Country:");
-            newAddress.Country = Console.ReadLine();
-            Console.WriteLine("Postal Code:");
-            newAddress.PostalCode = Console.ReadLine();
-            newCustomer.CustShipAddress = newAddress;
-            Console.WriteLine("Is the Customer's Billing Address the same as Shipping? (y/n):");
-            if (Console.ReadLine()[0] == 'y') {
-                newCustomer.CustBillAddress = newCustomer.CustShipAddress;
-            } else {
-                newAddress = new Address();
-                Console.WriteLine("Street:");
-                newAddress.Street = Console.ReadLine();
-                Console.WriteLine("City:");
-                newAddress.City = Console.ReadLine();
-                Console.WriteLine("State:");
-                newAddress.State = Console.ReadLine();
-                Console.WriteLine("Country:");
-                newAddress.Country = Console.ReadLine();
-                Console.WriteLine("Postal Code:");
-                newAddress.PostalCode = Console.ReadLine();
-                newCustomer.CustBillAddress = newAddress;
-            }
-            customerBL.AddCustomer(newCustomer);
-            foreach (var item in customerBL.GetCustomers())
-            {
-                Console.WriteLine(item.ToString());
-            }
+        {   
+            Boolean stay = true;
+            do {
+                Console.WriteLine("Welcome to my Store App! \nAre you a customer or manager?");
+                Console.WriteLine("[0] Customer");
+                Console.WriteLine("[1] Manager");
+                Console.WriteLine("[2] Exit App");
 
+                Console.WriteLine("Enter a number: ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput) {
+                    case "0":
+                        IMenu menu = new CustomerMenu(new CustomerBL(new CustomerRepoFile()));
+                        menu.Start();
+                        break;
+                    case "1":
+                        menu = new ManagerMenu();
+                        menu.Start();
+                        break;
+                    case "2":
+                        stay = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input! Please select a menu item");
+                        break;
+                }
+            } while (stay);
+            
+            /*
+            
+            */
         }
     }
 }
