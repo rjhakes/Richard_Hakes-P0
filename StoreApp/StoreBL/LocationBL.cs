340846d41baea0092428430446401615e6531d42
Rich Hakes <rjhakes@yahoo.com>
2021-02-17 21:37:40 -0700

    added interface implementations

--- StoreApp/StoreBL/LocationBL.cs
+++ StoreApp/StoreBL/LocationBL.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+using StoreDL;
+using StoreModels;
+
 namespace StoreBL
 {
-    public class LocationBL
+    public class LocationBL : ILocationBL
     {
-        
+        private ILocationRepository _repo;
+
+        public LocationBL(ILocationRepository repo) {
+            _repo = repo;
+        }
+
+        public void AddLocation(Location newLocation)
+        {
+            //TODO: Add BL
+            _repo.AddLocation(newLocation);
+        }
+        public List<Location> GetLocations()
+        {
+            //TODO Add BL
+            return _repo.GetLocations();
+        }
     }
 }
--- StoreApp/StoreBL/ProductBL.cs
+++ StoreApp/StoreBL/ProductBL.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+using StoreDL;
+using StoreModels;
+
 namespace StoreBL
 {
-    public class ProductBL
+    public class ProductBL : IProductBL
     {
-        
+        private IProductRepository _repo;
+
+        public ProductBL(IProductRepository repo) {
+            _repo = repo;
+        }
+
+        public void AddProduct(Product newProduct)
+        {
+            //TODO: Add BL
+            _repo.AddProduct(newProduct);
+        }
+        public List<Product> GetProducts()
+        {
+            //TODO Add BL
+            return _repo.GetProducts();
+        }
     }
 }
