256b69e19c6109084eaeb933a63f3ee4ac520214
Rich Hakes <rjhakes@yahoo.com>
2021-02-20 16:15:26 -0700

    enum States

---
+++ StoreApp/StoreModels/States.cs
@@ -0,0 +1,8 @@
+
+namespace StoreModels
+{
+    public enum States
+    {
+        Alabama, Alaska, DexcAmericanSamoa, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, DistrictofColumbia, Florida, Georgia, Guam, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, MinorOutlyingIslands, Mississippi, Missouri, Montana, Nebraska, Nevada, NewHampshire, NewJersey, NewMexico, NewYork, NorthCarolina, NorthDakota, NorthernMarianaIslands, Ohio, Oklahoma, Oregon, Pennsylvania, PuertoRico, RhodeIsland, SouthCarolina, SouthDakota, Tennessee, Texas, USVirginIslands, Utah, Vermont, Virginia, Washington, WestVirginia, Wisconsin, Wyoming, AK, AL, AR, AS, AZ, CA, CO, CT, DC, DE, FL, GA, GU, HI, IA, ID, IL, IN, KS, KY, LA, MA, MD, ME, MI, MN, MO, MP, MS, MT, NC, ND, NE, NH, NJ, NM, NV, NY, OH, OK, OR, PA, PR, RI, SC, SD, TN, TX, UM, UT, VA, VI, VT, WA, WI, WV, WY
+    }
+}
