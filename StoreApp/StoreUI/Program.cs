03bd5c384fd194ca5a2eef60198a17d83f090d2b
Rich Hakes <rjhakes@yahoo.com>
2021-02-20 16:17:14 -0700

    StoreFrontMenu.Start()

--- StoreApp/StoreUI/Program.cs
+++ StoreApp/StoreUI/Program.cs
@@ -7,39 +7,11 @@ namespace StoreUI
 {
     class Program
     {
+
         static void Main(string[] args)
-        {   
-            Boolean stay = true;
-            do {
-                Console.WriteLine("Welcome to my Store App! \nAre you a customer or manager?");
-                Console.WriteLine("[0] Customer");
-                Console.WriteLine("[1] Manager");
-                Console.WriteLine("[2] Exit App");
-
-                Console.WriteLine("Enter a number: ");
-                string userInput = Console.ReadLine();
-
-                switch (userInput) {
-                    case "0":
-                        IMenu menu = new CustomerMenu(new CustomerBL(new CustomerRepoFile()));
-                        menu.Start();
-                        break;
-                    case "1":
-                        menu = new ManagerMenu();
-                        menu.Start();
-                        break;
-                    case "2":
-                        stay = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input! Please select a menu item");
-                        break;
-                }
-            } while (stay);
-            
-            /*
-            
-            */
+        {     
+            IMenu menu = new StoreFrontMenu();
+            menu.Start();
         }
     }
 }
