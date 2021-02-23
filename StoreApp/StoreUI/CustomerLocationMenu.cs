3e244ef8bd1210b755accee322e97fbe4c50f794
Rich Hakes <rjhakes@yahoo.com>
2021-02-22 18:44:48 -0700

    added purchasing functionality

--- StoreApp/StoreUI/CustomerLocationMenu.cs
+++ StoreApp/StoreUI/CustomerLocationMenu.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using StoreModels;
 using StoreBL;
 using StoreDL;
@@ -9,6 +10,7 @@ namespace StoreUI
         private readonly string _menu;
         private IProductBL _productBL = new ProductBL(new ProductRepoFile());
         private ICustomerBL _customerBL = new CustomerBL(new CustomerRepoFile());
+        private IOrderBL _orderBL = new OrderBL(new OrderRepoFile());
         private ILocationBL _locationBL;
         private Location _storeLocation;
         private Customer _user;
@@ -21,7 +23,7 @@ namespace StoreUI
             _storeLocation = storeLocation;
             _user = user;
             _menu = "\n" +
-                    "\n[View] View Cart" +
+                    "\n[View] View Cart and finalize purchase" +
                     "\n[Back] Previous Menu" +
                     "\n[Exit] Exit App";
         }
@@ -41,7 +43,17 @@ namespace StoreUI
                 } else {
                     switch(userInput) {
                         case "View":
+                            //TODO move to CustomerCartMenu
                             ViewCart();
+                            if (_user.CustCart.Count > 0) {
+                                Console.WriteLine("Would you like to finalize your purchase? (y/n)"); 
+                                if (Console.ReadLine().Equals("y")) {
+                                    CompletePurchase();
+                                    Console.WriteLine("\nPurchase Complete!\nPress Enter to return to menu");
+                                    Console.ReadLine();
+                                }
+                            }
+                            
                             break;
                         case "Back":
                             stay = false;
@@ -68,9 +80,15 @@ namespace StoreUI
         }
 
         public void OrderItem(string _item) {
-            Console.WriteLine("In Order Item");
+            //move inventory quantity change to new method
             int i = int.Parse(_item);
-            _user.CustCart.Add(_storeLocation.Inventory[i]);
+            Item ChooseItem = new Item();
+            ChooseItem.Product = _storeLocation.Inventory[i].Product;
+            Console.Write($"Enter Quantity limited to {_storeLocation.Inventory[i].Quantity}: ");
+            ChooseItem.Quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine(ChooseItem.Quantity);
+            _storeLocation.Inventory[i].Quantity -= ChooseItem.Quantity;
+            _user.CustCart.Add(ChooseItem);
         }
 
         public void ViewCart() {
@@ -78,6 +96,44 @@ namespace StoreUI
                 Console.WriteLine(item);
             }
             Console.ReadLine();
+        }
+
+        public void CompletePurchase() {
+            LineItems();
+            Console.WriteLine(SumCharges());
+            CreateOrder();
+            EmptyCart();
+        }
+
+        public double SumCharges() {
+            double TotalCharge = 0;
+            foreach (var item in _user.CustCart) {
+                TotalCharge += item.Product.ProductPrice * item.Quantity;
+            }
+            return TotalCharge;
+        }
+
+        public void LineItems() {
+            int i = 0;
+            foreach (var item in _user.CustCart)
+            {
+                Console.WriteLine($"[{i}] {item.ToString()}");
+                i++;
+            }
+        }
+
+        public void CreateOrder() {
+            Order newOrder = new Order();
+            newOrder.Customer = _user;
+            newOrder.LocationName = _storeLocation.LocationName;
+            newOrder.LocationAddress = _storeLocation.Address;
+            newOrder.Cart = _user.CustCart;
+            newOrder.Total = SumCharges();
+            _orderBL.AddOrder(newOrder);
+        }
+
+        public void EmptyCart() {
+            _user.CustCart = new List<Item>();
         }
     }
 }
