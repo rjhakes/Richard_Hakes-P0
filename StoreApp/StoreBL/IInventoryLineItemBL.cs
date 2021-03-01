a3bde9562be83e2e458063f6ee5b236c2a7f58f7
Rich Hakes <rjhakes@yahoo.com>
2021-03-01 03:01:15 -0700

    Added InventoryLineItem BL

---
+++ StoreApp/StoreBL/IInventoryLineItemBL.cs
@@ -0,0 +1,14 @@
+using StoreModels;
+using System.Collections.Generic;
+
+namespace StoreBL
+{
+    public interface IInventoryLineItemBL
+    {
+        List<InventoryLineItem> GetInventoryLineItems();
+        void AddInventoryLineItem(InventoryLineItem newInventoryLineItem);
+        InventoryLineItem GetInventoryLineItemById(int id);
+        void DeleteInventoryLineItem(InventoryLineItem inventoryLineItem2BDeleted);
+        void UpdateInventoryLineItem(InventoryLineItem inventoryLineItem2BUpdated, InventoryLineItem updatedDetails);
+    }
+}
---
+++ StoreApp/StoreBL/InventoryLineItemBL.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using StoreDL;
+using StoreModels;
+
+namespace StoreBL
+{
+    public class InventoryLineItemBL : IInventoryLineItemBL
+    {
+        private IInventoryLineItemRepository _repo;
+
+        public InventoryLineItemBL(IInventoryLineItemRepository repo) {
+            _repo = repo;
+        }
+
+        public void AddInventoryLineItem(InventoryLineItem newInventoryLineItem)
+        {
+            //TODO: Add BL
+            _repo.AddInventoryLineItem(newInventoryLineItem);
+        }
+        public void DeleteInventoryLineItem(InventoryLineItem inventoryLineItem2BDeleted)
+        {
+            _repo.DeleteInventoryLineItem(inventoryLineItem2BDeleted);
+        }
+        public InventoryLineItem GetInventoryLineItemById(int id) {
+            //todo validate
+            return _repo.GetInventoryLineItemById(id);
+        }
+        public List<InventoryLineItem> GetInventoryLineItems()
+        {
+            //TODO Add BL
+            return _repo.GetInventoryLineItems();
+        }
+        public void UpdateInventoryLineItem(InventoryLineItem inventoryLineItem2BUpdated, InventoryLineItem updatedDetails)
+        {
+            inventoryLineItem2BUpdated.InventoryId = updatedDetails.InventoryId;
+            inventoryLineItem2BUpdated.ProductId = updatedDetails.ProductId;
+            inventoryLineItem2BUpdated.Quantity = updatedDetails.Quantity;
+            _repo.UpdateInventoryLineItem(inventoryLineItem2BUpdated);
+        }
+
+        
+    }
+}
