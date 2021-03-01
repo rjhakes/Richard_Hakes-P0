4813800103cdcfb017e299086e78cfa29932c64e
Rich Hakes <rjhakes@yahoo.com>
2021-03-01 03:02:30 -0700

    added productDB BL

--- StoreApp/StoreBL/IProductBL.cs
+++ StoreApp/StoreBL/IProductBL.cs
@@ -5,7 +5,11 @@ namespace StoreBL
 {
     public interface IProductBL
     {
-         List<Product> GetProducts();
-         void AddProduct(Product newProduct);
+        List<Product> GetProducts();
+        void AddProduct(Product newProduct);
+        Product GetProductByName(string name);
+        Product GetProductById(int id);
+        void DeleteProduct(Product product2BDeleted);
+        void UpdateProduct(Product product2BUpdated, Product updatedDetails);
     }
 }
--- StoreApp/StoreBL/ProductBL.cs
+++ StoreApp/StoreBL/ProductBL.cs
@@ -18,10 +18,30 @@ namespace StoreBL
             //TODO: Add BL
             _repo.AddProduct(newProduct);
         }
+        public void DeleteProduct(Product product2BDeleted)
+        {
+            _repo.DeleteProduct(product2BDeleted);
+        }
+        public Product GetProductByName(string name) {
+            //todo validate
+            return _repo.GetProductByName(name);
+        }
+        public Product GetProductById(int id) {
+            //todo validate
+            return _repo.GetProductById(id);
+        }
         public List<Product> GetProducts()
         {
             //TODO Add BL
             return _repo.GetProducts();
+        }
+        public void UpdateProduct(Product product2BUpdated, Product updatedDetails)
+        {
+            product2BUpdated.ProdName = updatedDetails.ProdName;
+            product2BUpdated.ProdPrice = updatedDetails.ProdPrice;
+            product2BUpdated.ProdCategory = updatedDetails.ProdCategory;
+            product2BUpdated.ProdBrandName = updatedDetails.ProdBrandName;
+            _repo.UpdateProduct(product2BUpdated);
         }
     }
 }
